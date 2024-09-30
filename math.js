8d2764d15019b4ea06875cd51ee2f2183fecd1b1
Yadvendra Singh Parmar <ydsingh555@gmail.com>
2024-09-30 16:39:12 +0530

    number and math done

---
+++ math.js
@@ -0,0 +1,29 @@
+console.log(Math.sqrt(25));
+console.log(Math.floor(5.8));
+console.log(Math.ceil(5.8));
+console.log(Math.round(5.8));
+
+
+
+console.log(Math.max(5,4,8,6,7));
+console.log(Math.min(5,4,8,6,7));
+
+
+
+
+let min = 10;
+let max = 20;
+console.log((Math.random() * 10) + 1);
+
+console.log(Math.floor(Math.random() * 10 + 1));
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
+
+
+
+
+
+
+
+
+
---
+++ number.js
@@ -0,0 +1,24 @@
+let age = 18;
+console.log(age);
+
+let myAge = new Number(18);
+console.log(myAge);
+
+console.log(myAge.toString().length);
+
+let price = 100;
+console.log(price.toFixed(2));
+
+let price2 = 123.8966
+console.log(price2.toPrecision(3));
+
+
+let maxPrice = 500000;
+console.log(maxPrice.toLocaleString("en-IN"));
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+
+
+
+
